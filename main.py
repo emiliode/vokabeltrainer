de728f2b88fe289db50ebed56d4744e09426b25f
emiliode <eb531@web.de>
2020-10-11 22:49:40 +0200

    some basic functions

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import pygame 
+from dearpygui.core import  *
+add_additional_font("arial.ttf", 20, )
+add_text("Vokabel trainer")
+def start_clicked(sender, data):
+    print("clicked: "+sender )
+add_button("start" , callback = start_clicked)
+
+start_dearpygui()
