29f0d5efe32d8bb1c38419fef5e48d2a60d79b9c
Samu <samu.brade@gmail.com>
2020-10-13 22:18:36 +0200

    jdhföa,famhgas

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from flask import  Flask , render_template , request
 import sqlite3
 app = Flask(__name__)
 
+print("hallo")
 @app.route("/", methods=["POST", "GET"])
 def hello():
     if request.method == "POST":
         print("\n Got: "+request.form["eingabe"])
     vocs = get_vocs("german-englisch")
     return render_template("index.html", voc=vocs[0][1])
-print("hallo")
 if __name__ == "__main__":
     app.run(debug=True)
